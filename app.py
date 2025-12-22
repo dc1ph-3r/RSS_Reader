b772746b1bccd99b6963e761fdad82592e36734f
dc1ph-3r <dcypher_1@proton.me>
2025-12-22 19:12:19 +0000

    adding dashboard functionality and updated index.html

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template # type: ignore
+import os
+from flask import Flask, render_template, jsonify # type: ignore
 from flask import send_from_directory # type: ignore
+from datetime import datetime, timezone
 import feedparser # type: ignore
 
 app = Flask(__name__)
 
 # list rss feeds to read from
-rss_feeds = {
+feed_url = {
     "BleepingComputer":"https://www.bleepingcomputer.com/feed/",
     "TechCrunch (All)":"https://techcrunch.com/feed/",
     "How-to-Geek":"https://howtogeek.com/feed/",
@@ -14,16 +16,22 @@ rss_feeds = {
     "CVEfeed.io":"https://cvefeed.io/rssfeed/latest.xml"
 } 
 
+def fetch_all_articles(feed_url):
+    all_articles = []
+    for url in feed_url.items():
+        feed = feedparser.parse(url)
+        for entry in feed.entries:
+            published = None
+            if hasattr(entry, "published_parsed"):
+                published = datetime(*entry.published_parsed[:6], tzinfo=timezone.utc)
 
-def get_feeds():
-    all_news = {}
-    for source, url in rss_feeds.items():
-        parsed = feedparser.parse(url)
-        if parsed.entries:
-            all_news[source] = parsed.entries[:5] # top 5 entries for each site
-        else:
-            all_news[source] = []
-    return all_news
+            all_articles.append({
+                "feed_title": name,
+                "title": entry.title,
+                "link": entry.link,
+                "published": published.isoformat() if published else None
+            })
+    return all_articles
 
 @app.route("/favicon.ico")
 def favicon():
@@ -35,8 +43,18 @@ def favicon():
 
 @app.route("/")
 def home():
-    news = get_feeds()
-    return render_template("index.html", news=news)
+    return render_template("index.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/api/dashboard")
+def api_dashboard():
+    # dashboard logic here
+    articles = fetch_all_articles()
+    articles.sort(key=lambda x: x["published"] or "", reverse=True)
+    return jsonify(articles)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
